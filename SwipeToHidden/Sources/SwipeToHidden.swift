5c676961f5f16511dc3a96ac583e2ecd1879de4b
Mazy <mazy_ios@163.com>
2019-01-09 17:35:16 +0800

    add SwipeToHidden project and implement it

---
+++ SwipeToHidden/Sources/SwipeToHidden.swift
@@ -0,0 +1,94 @@
+//
+//  SwipeToHidden.swift
+//  SwipeToHidden
+//
+//  Created by Mazy on 2019/1/9.
+//  Copyright © 2019 Mazy. All rights reserved.
+//
+
+import UIKit
+
+protocol SwipeToHiddenDelegate {
+    
+    /// Description
+    ///
+    /// - Parameters:
+    ///   - source: 源文件
+    ///   - interactive:
+    func swipeToHidden(_ source: SwipeToHidden, didUpdatePercentHiddenInteractively interactive: Bool)
+}
+
+class SwipeToHidden: NSObject {
+    
+    var delegate: SwipeToHiddenDelegate?
+    
+    var percentHidden: CGFloat = 0
+    var scrollDistance: CGFloat = 50
+    
+    private var  dragStartPosition: CGFloat = 0.0
+    private var isDragging: Bool = false
+    
+    func beginDragAtPosition(_ value: CGFloat) {
+        dragStartPosition = max(value, 0.0)
+        isDragging = true
+    }
+    
+    func endDragAtTargetPosition(_ value: CGFloat, velocity: CGFloat) {
+        isDragging = false
+        
+        var shouldHidden: Bool = true
+        if velocity < 0.0 || (velocity == 0.0 && percentHidden == 0.0) || (value < scrollDistance) {
+            shouldHidden = false
+        }
+        setPercentHidden(shouldHidden ? 1.0 : 0.0, interactive: false)
+    }
+    
+    func scrollToPosition(_ value: CGFloat) {
+        if value <= 0 {
+            setPercentHidden(0.0, interactive: false)
+        } else if isDragging {
+            if value < scrollDistance {
+                let newPercentHidden = value / scrollDistance
+                if newPercentHidden < percentHidden {
+                    setPercentHidden(newPercentHidden, interactive: true)
+                    return
+                }
+            }
+            
+            if percentHidden < 1.0 {
+                let diff = value - dragStartPosition
+                setPercentHidden(diff / scrollDistance, interactive: true)
+                if diff < 0.0 {
+                    dragStartPosition = value
+                }
+            }
+        }
+    }
+    
+    private func setPercentHidden(_ percent: CGFloat, interactive: Bool) {
+        
+        var newPercent = percent < 0 ? 0 : percent
+        newPercent  = percent > 1.0 ? 1.0 : percent
+        
+        if newPercent == percentHidden { return }
+        percentHidden = newPercent
+        
+        delegate?.swipeToHidden(self, didUpdatePercentHiddenInteractively: interactive)
+    }
+}
+
+// MARK: - UIScrollViewDelegate
+extension SwipeToHidden: UIScrollViewDelegate {
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        beginDragAtPosition(scrollView.contentOffset.y + scrollView.contentInset.top)
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        endDragAtTargetPosition(targetContentOffset.pointee.y + scrollView.contentInset.top, velocity: velocity.y)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        scrollToPosition(scrollView.contentOffset.y + scrollView.contentInset.top)
+    }
+}
