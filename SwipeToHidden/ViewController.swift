3c23e99c9ae28140989ad205eebe55664ed7e6b3
Mazy <mazy_ios@163.com>
2019-01-09 17:08:45 +0800

    init project

---
+++ SwipeToHidden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeToHidden
+//
+//  Created by Mazy on 2019/1/9.
+//  Copyright © 2019 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
